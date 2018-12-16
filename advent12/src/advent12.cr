14f4f0cd17ed42ac385ec9fac5c7d85994947920
juumixx <juumixx@gmail.com>
2018-12-16 10:17:05 +0100

    advent12 part1 working

--- advent12/src/advent12.cr
+++ advent12/src/advent12.cr
@@ -20,7 +20,7 @@ module Advent12
 .####
 #.###
 #####".lines
-
+  offset = 5
   generations = 20
 
   def to_bitarray(s : String, size : Int32?)
@@ -32,26 +32,28 @@ module Advent12
     return a
   end
 
-  f = to_bitarray g, 1000
+  f = to_bitarray g, 150
   a = alive.map { |l| to_bitarray l, nil }
 
   def next_gen(f : BitArray, a : Array(BitArray))
     n = BitArray.new f.size
     (0...(f.size - 4)).each do |i|
       cut = f[i, 5]
-      if a.any? { |z| z == cut }
+      if a.any? { |z| z.zip(cut).all? do |q, w| q == w end }
         n[i + 2] = true
       end
     end
     return n
   end
 
-  puts f
+  puts "0 #{f}"
   generations.times do |i|
     f = next_gen(f, a)
-    puts "#{i+1} #{f}"
+    if !(i % 50000000)
+      puts "#{i*100/generations}"
+    end
   end
   sum = 0
-  puts f.each_with_index { |t, i| sum += i-5 if t }
+  puts f.each_with_index { |t, i| sum += i-offset if t }
   puts sum
 end
